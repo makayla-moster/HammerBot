4305cb6a6b1c93fbf2b955c99a6271d2b2fd5dc4
makayla-moster <mmoster@clemson.edu>
2022-01-17 21:19:04 -0500

    [Cogs] - Updated Error Handling

--- cog_modules/error_handler/cog.py
+++ cog_modules/error_handler/cog.py
@@ -17,6 +17,16 @@ class ErrorHandler(commands.Cog):
             message = discord.Embed(title="Command on Cooldown", description=f"This command is on cooldown. Please try again after {round(error.retry_after, 1)} seconds.", color=discord.Color.red())
         elif isinstance(error, commands.MissingPermissions):
             message = discord.Embed(title="Missing Permissions", description="You are missing the required permissions to run this command.", color = discord.Color.red())
+        elif isinstance(error, commands.BadArgument):
+            message = discord.Embed(title="Bad Argument", description="You have input an invalid parameter for this command.", color = discord.Color.red())
+        elif isinstance(error, commands.ExpectedClosingQuoteError):
+            message = discord.Embed(title="Missing Quotes", description="Quotes are required for multi-word arguments for this command.", color = discord.Color.red())
+        elif isinstance(error, commands.ArgumentParsingError):
+            message = discord.Embed(title="Argument Error", description="Looks like that argument didn't work. Please try again with a different one.", color = discord.Color.red())
+        elif isinstance(error, commands.TooManyArguments):
+            message = discord.Embed(title="Too Many Arguments", description="You have too many arguments for the command. Please try again with less arguments.", color = discord.Color.red())
+        elif isinstance(error, commands.UserInputError):
+            message = discord.Embed(title='Input Error', description="There was a problem with your input. Please check your input and try again.", color = discord.Color.red())
         else:
             message = discord.Embed(title="Tell @quela about this", description="If you are seeing this, something has gone horribly wrong.", color = discord.Color.red())
 
