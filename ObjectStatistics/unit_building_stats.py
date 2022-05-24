7a5493022385929e12c6b7f913e601d5ab90e39a
Michael Byrd <mbyrdsmith94@gmail.com>
2022-05-24 16:44:40 -0400

    Created Unit building json

--- ObjectStatistics/unit_building_stats.py
+++ ObjectStatistics/unit_building_stats.py
@@ -7,16 +7,6 @@ from unit_building_class import unit_building
 with open("directory.json") as fp:
     directory = json.load(fp)
 
-
-
-x = "4"
-
-obj = unit_building(directory["units_buildings"][x])
-
-# print(obj.get_cost())
-#
-# print(obj)
-
 unit_building_dict = {}
 
 for item in directory["units_buildings"]:
@@ -24,7 +14,27 @@ for item in directory["units_buildings"]:
     unit_building_dict[unit_building_obj.get_name()] = unit_building_obj
 
 
-# save obj to file
-a_file = open("unit_build_stats.pkl", "wb")
-pickle.dump(unit_building_dict, a_file)
-a_file.close()
+json_dict = {}
+
+for key, value in unit_building_dict.items():
+    json_dict[value.get_name()] = {"cost": value.get_cost(),
+    "attack" : value.get_attack(),
+    "melee_armor": value.get_melee_armor(),
+    "pierce_armor": value.get_pierce_armor(),
+    "hit_points": value.get_hit_points(),
+    "los": value.get_los()}
+
+# Serializing json
+json_object = json.dumps(json_dict, indent = 4)
+
+# Writing to sample.json
+with open("unit_building.json", "w") as outfile:
+    outfile.write(json_object)
+
+
+
+
+# # save obj to file
+# a_file = open("unit_build_stats.pkl", "wb")
+# pickle.dump(unit_building_dict, a_file)
+# a_file.close()
