9b2ec3ab132fba55133f39f35e9a1eb13cdff846
Michael Byrd <mbyrdsmith94@gmail.com>
2023-04-23 17:19:52 -0400

    Fixed stats cog

--- cog_modules/stats/cog.py
+++ cog_modules/stats/cog.py
@@ -43,12 +43,13 @@ class StatCommands(commands.Cog):
             )
             await ctx.send(embed=embed)
         else:
-            if arg1.title() in localised_tech_name_lookup:
-                unitBuildNum = localised_tech_name_lookup[arg1.title()]
+
+            if input in localised_tech_name_lookup:
+                unitBuildNum = localised_tech_name_lookup[input]
                 entity = "techs"
                 entityDirectory = "Techs"
-            if arg1.title() in localised_unit_building_name_lookup:
-                unitBuildNum = localised_unit_building_name_lookup[arg1.title()]
+            if input in localised_unit_building_name_lookup:
+                unitBuildNum = localised_unit_building_name_lookup[input]
                 if unitBuildNum in techtree["data"]["units"]:
                     entity = "units"
                     entityDirectory = "Units"
@@ -56,7 +57,7 @@ class StatCommands(commands.Cog):
                     entity = "buildings"
                     entityDirectory = "Buildings"
 
-            cost = get_cost(arg1.title(), entity)
+            cost = get_cost(input, entity)
             costString = ""
             if "Food" in cost:
                 costString += f"{cost['Food']} <:food:978788983377121311> "
@@ -71,13 +72,13 @@ class StatCommands(commands.Cog):
             else:
                 costString2 = "No resources needed."
 
-            if entity != "techs" and arg1.title()[-1] != "s":
+            if entity != "techs" and input[-1] != "s":
                 embed = disnake.Embed(
-                    title=f"{arg1.title()} Stats", description=f"Information about {arg1.title()}s.", color=0xD5D341
+                    title=f"{input} Stats", description=f"Information about {input}s.", color=0xD5D341
                 )
             else:
                 embed = disnake.Embed(
-                    title=f"{arg1.title()} Stats", description=f"Information about {arg1.title()}.", color=0xD5D341
+                    title=f"{input} Stats", description=f"Information about {input}.", color=0xD5D341
                 )
             embed.set_thumbnail(
                 url=f"https://raw.githubusercontent.com/SiegeEngineers/aoe2techtree/master/img/{entityDirectory}/{unitBuildNum}.png"
@@ -86,26 +87,26 @@ class StatCommands(commands.Cog):
 
             if entity == "techs":
                 embed.add_field(name="Research Time", value=get_tech_researchTime(unitBuildNum), inline=True)
-                embed.add_field(name="Description", value=f"{descriptions[arg1.title()]}", inline=False)
+                embed.add_field(name="Description", value=f"{descriptions[input]}", inline=False)
 
             else:
-                embed.add_field(name="Hit Points", value=f"{get_HP(arg1.title(), entity)}", inline=True)
-                if "Base Melee" in get_attacks(arg1.title(), entity):
+                embed.add_field(name="Hit Points", value=f"{get_HP(input, entity)}", inline=True)
+                if "Base Melee" in get_attacks(input, entity):
                     embed.add_field(
-                        name="Base Melee Damage", value=f"{get_attacks(arg1.title(), entity)['Base Melee']}", inline=True
+                        name="Base Melee Damage", value=f"{get_attacks(input, entity)['Base Melee']}", inline=True
                     )
-                if "Base Pierce" in get_attacks(arg1.title(), entity):
+                if "Base Pierce" in get_attacks(input, entity):
                     embed.add_field(
-                        name="Base Pierce Damage", value=f"{get_attacks(arg1.title(), entity)['Base Pierce']}", inline=True
+                        name="Base Pierce Damage", value=f"{get_attacks(input, entity)['Base Pierce']}", inline=True
                     )
-                embed.add_field(name="Melee Armor", value=f"{get_armours(arg1.title(), entity)['Base Melee']}", inline=True)
-                embed.add_field(name="Pierce Armor", value=f"{get_armours(arg1.title(), entity)['Base Pierce']}", inline=True)
-                if get_range(arg1.title(), entity)["Range"] != 0:
-                    embed.add_field(name="Range", value=f"{get_range(arg1.title(), entity)['Range']} tiles", inline=True)
-                if get_speed(arg1.title(), entity) != 0:
-                    embed.add_field(name="Speed", value=f"{get_speed(arg1.title(), entity)} tiles/sec", inline=True)
+                embed.add_field(name="Melee Armor", value=f"{get_armours(input, entity)['Base Melee']}", inline=True)
+                embed.add_field(name="Pierce Armor", value=f"{get_armours(input, entity)['Base Pierce']}", inline=True)
+                if get_range(input, entity)["Range"] != 0:
+                    embed.add_field(name="Range", value=f"{get_range(input, entity)['Range']} tiles", inline=True)
+                if get_speed(input, entity) != 0:
+                    embed.add_field(name="Speed", value=f"{get_speed(input, entity)} tiles/sec", inline=True)
             embed.set_footer(
-                text=f"{ctx.author.name}\nFor more information run !advstats {arg1.title()}",
+                text=f"{ctx.author.name}\nFor more information run !advstats {input}",
                 icon_url=ctx.author.display_avatar.url,
             )
             await ctx.send(embed=embed)
@@ -135,12 +136,12 @@ class StatCommands(commands.Cog):
             )
             await ctx.send(embed=embed)
         else:
-            if arg1.title() in localised_tech_name_lookup:
-                unitBuildNum = localised_tech_name_lookup[arg1.title()]
+            if input in localised_tech_name_lookup:
+                unitBuildNum = localised_tech_name_lookup[input]
                 entity = "techs"
                 entityDirectory = "Techs"
-            if arg1.title() in localised_unit_building_name_lookup:
-                unitBuildNum = localised_unit_building_name_lookup[arg1.title()]
+            if input in localised_unit_building_name_lookup:
+                unitBuildNum = localised_unit_building_name_lookup[input]
                 if unitBuildNum in techtree["data"]["units"]:
                     entity = "units"
                     entityDirectory = "Units"
@@ -148,7 +149,7 @@ class StatCommands(commands.Cog):
                     entity = "buildings"
                     entityDirectory = "Buildings"
 
-            cost = get_cost(arg1.title(), entity)
+            cost = get_cost(input, entity)
 
             costString = ""
             if "Food" in cost:
@@ -164,13 +165,13 @@ class StatCommands(commands.Cog):
             else:
                 costString2 = "No resources needed."
 
-            if entity != "techs" and arg1.title()[-1] != "s":
+            if entity != "techs" and input[-1] != "s":
                 embed = disnake.Embed(
-                    title=f"{arg1.title()} Stats", description=f"Information about {arg1.title()}s.", color=0xD5D341
+                    title=f"{input} Stats", description=f"Information about {input}s.", color=0xD5D341
                 )
             else:
                 embed = disnake.Embed(
-                    title=f"{arg1.title()} Stats", description=f"Information about {arg1.title()}.", color=0xD5D341
+                    title=f"{input} Stats", description=f"Information about {input}.", color=0xD5D341
                 )
 
             embed.set_thumbnail(
@@ -180,57 +181,57 @@ class StatCommands(commands.Cog):
 
             if entity == "techs":
                 embed.add_field(name="Research Time", value=get_tech_researchTime(unitBuildNum), inline=True)
-                embed.add_field(name="Description", value=f"{descriptions[arg1.title()]}", inline=False)
+                embed.add_field(name="Description", value=f"{descriptions[input]}", inline=False)
             else:
-                embed.add_field(name="Hit Points", value=f"{get_HP(arg1.title(), entity)}", inline=True)
-                if "Base Melee" in get_attacks(arg1.title(), entity):
+                embed.add_field(name="Hit Points", value=f"{get_HP(input, entity)}", inline=True)
+                if "Base Melee" in get_attacks(input, entity):
                     embed.add_field(
-                        name="Base Melee Damage", value=f"{get_attacks(arg1.title(), entity)['Base Melee']}", inline=True
+                        name="Base Melee Damage", value=f"{get_attacks(input, entity)['Base Melee']}", inline=True
                     )
-                if "Base Pierce" in get_attacks(arg1.title(), entity):
+                if "Base Pierce" in get_attacks(input, entity):
                     embed.add_field(
-                        name="Base Pierce Damage", value=f"{get_attacks(arg1.title(), entity)['Base Pierce']}", inline=True
-                    )
-
-                if "Base Melee" in get_armours(arg1.title(), entity):
-                    embed.add_field(
-                        name="Base Melee Armor", value=f"{get_armours(arg1.title(), entity)['Base Melee']}", inline=True
-                    )
-                if "Base Pierce" in get_armours(arg1.title(), entity):
-                    embed.add_field(
-                        name="Base Pierce Armor", value=f"{get_armours(arg1.title(), entity)['Base Pierce']}", inline=True
+                        name="Base Pierce Damage", value=f"{get_attacks(input, entity)['Base Pierce']}", inline=True
                     )
 
-                if get_range(arg1.title(), entity)["Range"] != 0:
-                    embed.add_field(name="Range", value=f"{get_range(arg1.title(), entity)['Range']} tiles", inline=True)
-                if get_range(arg1.title(), entity)["Minimum Range"] != 0:
+                if "Base Melee" in get_armours(input, entity):
                     embed.add_field(
-                        name="Minimum Range", value=f"{get_range(arg1.title(), entity)['Minimum Range']} tiles", inline=True
+                        name="Base Melee Armor", value=f"{get_armours(input, entity)['Base Melee']}", inline=True
                     )
-                for key in get_attacks(arg1.title(), entity):
+                if "Base Pierce" in get_armours(input, entity):
+                    embed.add_field(
+                        name="Base Pierce Armor", value=f"{get_armours(input, entity)['Base Pierce']}", inline=True
+                    )
+
+                if get_range(input, entity)["Range"] != 0:
+                    embed.add_field(name="Range", value=f"{get_range(input, entity)['Range']} tiles", inline=True)
+                if get_range(input, entity)["Minimum Range"] != 0:
+                    embed.add_field(
+                        name="Minimum Range", value=f"{get_range(input, entity)['Minimum Range']} tiles", inline=True
+                    )
+                for key in get_attacks(input, entity):
                     if key != ("Base Melee") and key != ("Base Pierce"):
-                        embed.add_field(name=f"{key} Damage", value=f"{get_attacks(arg1.title(), entity)[key]}", inline=True)
-                for key in get_armours(arg1.title(), entity):
+                        embed.add_field(name=f"{key} Damage", value=f"{get_attacks(input, entity)[key]}", inline=True)
+                for key in get_armours(input, entity):
                     if key != ("Anti-Leitis") and key != ("Base Melee") and key != ("Base Pierce"):
-                        embed.add_field(name=f"{key} Armor", value=f"{get_armours(arg1.title(), entity)[key]}", inline=True)
-                if get_attacks(arg1.title(), entity):
-                    embed.add_field(name="Accuracy", value=f"{get_accuracy(arg1.title(), entity)}%", inline=True)
+                        embed.add_field(name=f"{key} Armor", value=f"{get_armours(input, entity)[key]}", inline=True)
+                if get_attacks(input, entity):
+                    embed.add_field(name="Accuracy", value=f"{get_accuracy(input, entity)}%", inline=True)
                     if entity == "units":
                         embed.add_field(
                             name="Attack Delay",
-                            value=f"{round(float(get_attackDelay(arg1.title(), entity)), 2)} sec",
+                            value=f"{round(float(get_attackDelay(input, entity)), 2)} sec",
                             inline=True,
                         )
                     if entity == "units":
-                        embed.add_field(name="Frame Delay", value=f"{get_frameDelay(arg1.title(), entity)}", inline=True)
-                    embed.add_field(name="Reload Time", value=f"{get_reloadTime(arg1.title(), entity)} sec", inline=True)
-                embed.add_field(name="Line of Sight", value=f"{get_LineOfSight(arg1.title(), entity)} tiles", inline=True)
-                if get_speed(arg1.title(), entity) != 0:
-                    embed.add_field(name="Speed", value=f"{get_speed(arg1.title(), entity)} tiles/sec", inline=True)
+                        embed.add_field(name="Frame Delay", value=f"{get_frameDelay(input, entity)}", inline=True)
+                    embed.add_field(name="Reload Time", value=f"{get_reloadTime(input, entity)} sec", inline=True)
+                embed.add_field(name="Line of Sight", value=f"{get_LineOfSight(input, entity)} tiles", inline=True)
+                if get_speed(input, entity) != 0:
+                    embed.add_field(name="Speed", value=f"{get_speed(input, entity)} tiles/sec", inline=True)
                 if entity == "buildings":
-                    embed.add_field(name="Build Time", value=f"{get_trainTime(arg1.title(), entity)} sec", inline=True)
+                    embed.add_field(name="Build Time", value=f"{get_trainTime(input, entity)} sec", inline=True)
                 if entity == "units":
-                    embed.add_field(name="Train Time", value=f"{get_trainTime(arg1.title(), entity)} sec", inline=True)
+                    embed.add_field(name="Train Time", value=f"{get_trainTime(input, entity)} sec", inline=True)
             if unitBuildNum in techtree["data"]["unit_upgrades"]:
                 cost = get_upgrade_cost(unitBuildNum)
                 costString = ""
@@ -251,7 +252,7 @@ class StatCommands(commands.Cog):
                 embed.add_field(name="Upgrade Research Time", value=f"{get_upgrade_researchTime(unitBuildNum)}", inline=True)
 
             embed.set_footer(
-                text=f"{ctx.author.name}\nFor less information run !stats {arg1.title()}",
+                text=f"{ctx.author.name}\nFor less information run !stats {input}",
                 icon_url=ctx.author.display_avatar.url,
             )
             await ctx.send(embed=embed)
