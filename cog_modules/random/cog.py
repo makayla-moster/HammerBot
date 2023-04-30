a7bf02e192b0eb6ec04b5035411cafcc545900b6
makayla-moster <mmoster@clemson.edu>
2023-04-29 21:34:18 -0400

    cat cog

---
+++ cog_modules/random/cog.py
@@ -0,0 +1,20 @@
+import asyncio
+import disnake
+import requests
+from disnake.ext import commands
+
+class Random(commands.Cog):
+
+    def __init__(self, bot: commands.Bot):
+        self.bot = bot
+
+    @commands.command(name="!cat", aliases=["!cotd", "!cats"])
+    async def cat(self, ctx: commands.Context):
+        url = "https://api.thecatapi.com/v1/images/search"
+        response = requests.get(url, timeout=5)
+        res = response.json()
+        await ctx.send(res[0]["url"])
+
+
+def setup(bot: commands.Bot):
+    bot.add_cog(Random(bot))
