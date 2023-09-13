bda5abef78cfbe55a2205f6b82a36bd00747bbf0
makayla-moster <mmoster@clemson.edu>
2023-09-13 18:26:30 -0400

    u

--- cog_modules/taunts/cog.py
+++ cog_modules/taunts/cog.py
@@ -134,7 +134,7 @@ class Taunts(commands.Cog):
             embed.add_field(name="Gold", value="0" + "<:gold:978788983364546581>", inline=True)
             embed.add_field(name="Stone", value="0" + "<:stone:978788984547315792>", inline=True)
             embed.set_footer(
-                text=f"{ctx.author.display_name}\nTo get more resources type `38` in chat.",
+                text=f"{ctx.author.display_name}\nTo get or lose more resources type `38` in chat.",
                 icon_url=ctx.author.display_avatar.url,
             )
 
@@ -156,7 +156,7 @@ class Taunts(commands.Cog):
         if user in serverResources:
             currentRes = serverResources[user][res]
             serverResources[user][res] = str(int(currentRes) + int(num))
-            response += f"\n You now have ||{serverResources[user][res]}|| {res} "
+            response += f"\nYou now have a total of ||{serverResources[user][res]}|| {res}."
         else:
             serverResources[user] = {"Food": "0", "Wood": "0", "Gold": "0", "Stone": "0"}
             serverResources[user][res] = str(num)
@@ -165,7 +165,7 @@ class Taunts(commands.Cog):
     @no_38.error
     async def no_38_error(self, ctx, error):
         if isinstance(error, commands.CommandOnCooldown):
-            await ctx.send(f"You can only get resources every 30 seconds. Try again in {round(error.retry_after, 2)} seconds.")
+            await ctx.send(f"You can only get or lose resources every 30 seconds. Try again in {round(error.retry_after, 2)} seconds.")
 
     @commands.command(name="58")
     async def no_58(self, ctx: commands.Context):
