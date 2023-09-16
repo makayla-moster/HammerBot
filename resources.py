2a323e5474e4f5f71dcd20f937b85c3820fc348c
makayla-moster <mmoster@clemson.edu>
2023-09-15 20:57:27 -0400

    u

--- resources.py
+++ resources.py
@@ -1,10 +1,5 @@
 from collections import Counter
 
-serverResources = {
-    "quela": {"Food": "-1068", "Wood": "449", "Gold": "-826", "Stone": "-148"},
-    "bshammer": {"Food": "-463", "Wood": "-622", "Gold": "-388", "Stone": "1093"},
-    "probablybutter": {"Food": "295", "Wood": "-2024", "Gold": "-1866", "Stone": "-2886"},
-    "olaf_the_shrew": {"Food": "323", "Wood": "0", "Gold": "0", "Stone": "0"},
-    ".harristotle": {"Food": "-1448", "Wood": "560", "Gold": "0", "Stone": "-706"},
-}
+serverResources = {'quela': {'Food': '-1068', 'Wood': '449', 'Gold': '-826', 'Stone': '12'}, 'bshammer': {'Food': '-1250', 'Wood': '-622', 'Gold': '-850', 'Stone': '1942'}, 'probablybutter': {'Food': '295', 'Wood': '-2024', 'Gold': '-1006', 'Stone': '-2044'}, 'olaf_the_shrew': {'Food': '323', 'Wood': '0', 'Gold': '0', 'Stone': '0'}, '.harristotle': {'Food': '-2241', 'Wood': '560', 'Gold': '-986', 'Stone': '-706'}}
+
 gizmoResources = Counter()
