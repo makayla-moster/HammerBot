dad2ad93c7a886840ffb8203ef2acaac8d13d457
ProbablyButter <probablybutter@gmail.com>
2023-10-08 09:36:32 -0700

    wrong resource lookup

--- cog_modules/random/cog.py
+++ cog_modules/random/cog.py
@@ -75,7 +75,7 @@ class Random(commands.Cog):
         most_common = taoResources.most_common(2)
         if most_common[0][1] != most_common[1][1] and pic == most_common[0][0]:
             info += f" HammerBot's favorite Tao pic! Shown {most_common[0][1]} times."
-        elif gizmoResources[pic] == 1:
+        elif taoResources[pic] == 1:
             info += " First time shown! ^_^"
         else:
             info += f" Shown {taoResources[pic]} times."
